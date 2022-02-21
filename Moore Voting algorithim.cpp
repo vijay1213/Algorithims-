1f9565fe93ce43fb267beb6cc0271bc6c64800b2
Vijay Chathe <76934330+vijay1213@users.noreply.github.com>
2022-02-21 20:36:16 +0530

    Create Moore Voting algorithim.cpp

---
+++ Moore Voting algorithim.cpp
@@ -0,0 +1,12 @@
+int majorityElement(vector<int> &num) {
+    int majorityIndex = 0;
+    for (int count = 1, i = 1; i < num.size(); i++) {
+        num[majorityIndex] == num[i] ? count++ : count--;
+        if (count == 0) {
+            majorityIndex = i;
+            count = 1;
+        }
+    }
+        
+    return num[majorityIndex];
+}
